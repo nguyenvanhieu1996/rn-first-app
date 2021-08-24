4dc94a7d81cd6635b86cbd9b43b8f6bb803cae98
nguyenvanhieu1996 <nguyenhieu4477@gmail.com>
2021-08-24 21:55:19 +0700

    .

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-const store = configureStore();
+const store = configureStore()
 import AppHeader from './src/Navigation/AppHeader';
 import { Provider } from 'react-redux'
 import store from './src/Store'

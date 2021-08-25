06ba4b056e1321ad013a701ae270ca6c6d0d483c
nguyenvanhieu1996 <nguyenhieu4477@gmail.com>
2021-08-25 08:57:10 +0700

    .

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-const store = configureStore()
 import AppHeader from './src/Navigation/AppHeader';
 import { Provider } from 'react-redux'
 import store from './src/Store'
 import { PersistGate } from 'redux-persist/integration/react'
-
+import {} from '@babel/core'
+const store = configureStore();
 const App = () => {
   return (
     <Provider store={store}>
